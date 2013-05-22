22d3a19da2fce3ec5f6d87c5328b646970750444
Aleksi Sapon <alek42008@yahoo.com>
2013-05-22 15:18:01 -0400

    Port Matrix3x3. Improve the Vector3 port.
    Assignment operators should be ported as instance methods.
    Normal operators should be static methods.
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/jreactphysics3d/mathematics/Vector3.java
+++ src/main/java/org/spout/jreactphysics3d/mathematics/Vector3.java
@@ -32,38 +32,33 @@ package org.spout.jreactphysics3d.mathematics;
  */
 public class Vector3 {
 	/**
-	 * X_AXIS, represents the x axis in the vector.
-	 * Value of 0
+	 * X_AXIS, represents the x axis in the vector. Value of 0
 	 */
 	public static final int X_AXIS = 0;
 	/**
-	 * Y_AXIS, represents the y axis in the vector.
-	 * Value of 1
+	 * Y_AXIS, represents the y axis in the vector. Value of 1
 	 */
 	public static final int Y_AXIS = 1;
 	/**
-	 * Z_AXIS, represents the z axis in the vector.
-	 * Value of 2
+	 * Z_AXIS, represents the z axis in the vector. Value of 2
 	 */
 	public static final int Z_AXIS = 2;
-
 	private float x;
 	private float y;
 	private float z;
 
 	/**
-	 * Default constructor.
-	 * All values are 0.0F
+	 * Default constructor. All values are 0.0F
 	 */
 	public Vector3() {
-		x = 0.0F;
-		y = 0.0F;
-		z = 0.0F;
+		x = 0;
+		y = 0;
+		z = 0;
 	}
 
 	/**
 	 * Constructor with arguments.
-	 * 
+	 *
 	 * @param x value
 	 * @param y value
 	 * @param z value
@@ -76,7 +71,7 @@ public class Vector3 {
 
 	/**
 	 * Copy constructor
-	 * 
+	 *
 	 * @param vector to copy
 	 */
 	public Vector3(Vector3 vector) {
@@ -87,7 +82,7 @@ public class Vector3 {
 
 	/**
 	 * Sets the x value of the vector
-	 * 
+	 *
 	 * @param x value to set
 	 */
 	public void setX(float x) {
@@ -96,7 +91,7 @@ public class Vector3 {
 
 	/**
 	 * Sets the y value of the vector
-	 * 
+	 *
 	 * @param y value to set
 	 */
 	public void setY(float y) {
@@ -105,7 +100,7 @@ public class Vector3 {
 
 	/**
 	 * Sets the z value of the vector
-	 * 
+	 *
 	 * @param z value to set
 	 */
 	public void setZ(float z) {
@@ -114,7 +109,7 @@ public class Vector3 {
 
 	/**
 	 * Sets all values of the vector
-	 * 
+	 *
 	 * @param x value to set
 	 * @param y value to set
 	 * @param z value to set
@@ -127,7 +122,7 @@ public class Vector3 {
 
 	/**
 	 * Gets the x value of the vector
-	 * 
+	 *
 	 * @return {@link float} x value
 	 */
 	public float getX() {
@@ -136,7 +131,7 @@ public class Vector3 {
 
 	/**
 	 * Gets the y value of the vector
-	 * 
+	 *
 	 * @return {@link float} y value
 	 */
 	public float getY() {
@@ -145,7 +140,7 @@ public class Vector3 {
 
 	/**
 	 * Gets the z value of the vector
-	 * 
+	 *
 	 * @return {@link float} z value
 	 */
 	public float getZ() {
@@ -153,8 +148,17 @@ public class Vector3 {
 	}
 
 	/**
+	 * Sets the x, y and z values to zero.
+	 */
+	public void setToZero() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	/**
 	 * Return the length of the vector
-	 * 
+	 *
 	 * @return {@link float} length of the vector
 	 */
 	public float length() {
@@ -163,7 +167,7 @@ public class Vector3 {
 
 	/**
 	 * Return the square of the length of the vector
-	 * 
+	 *
 	 * @return {@link float} square length of the vector
 	 */
 	public float lengthSquare() {
@@ -171,37 +175,8 @@ public class Vector3 {
 	}
 
 	/**
-	 * Scalar product of two vectors
-	 * 
-	 * @param vector to compute scalar product with
-	 * @return {@link float} scalar product
-	 */
-	public float dot(Vector3 vector) {
-		return x * vector.getX() + y * vector.getY() + z * vector.getZ();
-	}
-
-	/**
-	 * Cross product of two vectors
-	 * 
-	 * @param vector to computer cross product with
-	 * @return new {@link Vector3} cross product
-	 */
-	public Vector3 cross(Vector3 vector) {
-		return new Vector3(this.y * vector.z - this.z * vector.y, this.z * vector.x - this.x * vector.z, this.x * vector.y - this.y * vector.x);
-	}
-
-	/**
-	 * Return the corresponding absolute value vector
-	 * 
-	 * @return new {@link Vector3} absolute value vector
-	 */
-	public Vector3 getAbsoluteVector() {
-		return new Vector3(Math.abs(x), Math.abs(y), Math.abs(z));
-	}
-
-	/**
 	 * Return true if two vectors are parallel
-	 * 
+	 *
 	 * @param vector to compare with
 	 * @return true if the two vectors are parallel
 	 */
@@ -211,7 +186,7 @@ public class Vector3 {
 
 	/**
 	 * Return the axis with the minimal value
-	 * 
+	 *
 	 * @return {@link int} axis with minimal value
 	 */
 	public int getMinAxis() {
@@ -220,7 +195,7 @@ public class Vector3 {
 
 	/**
 	 * Return the axis with the maximum value
-	 * 
+	 *
 	 * @return {@link int} axis with maximum value
 	 */
 	public int getMaxAxis() {
@@ -229,135 +204,229 @@ public class Vector3 {
 
 	/**
 	 * True if the vector is unit, otherwise false
-	 * 
+	 *
 	 * @return true if the vector is unit, otherwise false
 	 */
 	public boolean isUnit() {
-		return Mathematics.approxEquals(lengthSquare(), 1.0F);
+		return Mathematics.approxEquals(lengthSquare(), 1);
 	}
 
 	/**
 	 * True if the vector is the zero vector
-	 * 
+	 *
 	 * @return true if the vector is the zero vector
 	 */
 	public boolean isZero() {
-		return Mathematics.approxEquals(lengthSquare(), 0.0F);
+		return Mathematics.approxEquals(lengthSquare(), 0);
 	}
 
 	/**
-	 * Return the corresponding unit vector
-	 * 
+	 * Return the corresponding unit vector. Creates a new vector.
+	 *
 	 * @return new unit {@link Vector3} corresponding to this vector
 	 */
 	public Vector3 getUnit() {
-		if (this.isZero()) {
+		if (isZero()) {
 			throw new UnsupportedOperationException("Unable to get the unit vector for the zero vector");
 		}
 		final float lengthVector = length();
-		final float lengthInv = 1.0F / lengthVector;
+		final float lengthInv = 1 / lengthVector;
 		return new Vector3(x * lengthInv, y * lengthInv, z * lengthInv);
 	}
 
 	/**
 	 * Return an orthogonal vector of this vector
-	 * 
+	 *
 	 * @return an orthogonal {@link Vector3} of the current vector
 	 */
 	public Vector3 getOneOrthogonalVector() {
-		if (this.isZero()) {
+		if (isZero()) {
 			throw new UnsupportedOperationException("Unable to get an orthogonal vector for the zero vector");
 		}
 
 		Vector3 vector = new Vector3();
-		if (!Mathematics.approxEquals(x, 0.0F)) { // if x != 0
+		if (!Mathematics.approxEquals(x, 0)) { // if x != 0
 			vector.setY(x);
 			vector.setZ((-2 * x * y * z + 2 * x * z) / (2 * (z * z + x * x)));
-			vector.setX((-x * y - z * vector.z) / x);
+			vector.setX((-x * y - z * vector.getZ()) / x);
 			return vector;
 		}
-		if (!Mathematics.approxEquals(y, 0.0F)) { // if y != 0
+		if (!Mathematics.approxEquals(y, 0)) { // if y != 0
 			vector.setZ(y);
 			vector.setX((-2 * x * y * z + 2 * x * y) / (2 * (y * y + x * x)));
-			vector.setY((-z * y - x * vector.x) / y);
+			vector.setY((-z * y - x * vector.getX()) / y);
 			return vector;
 		}
-		if (!Mathematics.approxEquals(z, 0.0F)) { // if z != 0
+		if (!Mathematics.approxEquals(z, 0)) { // if z != 0
 			vector.setX(z);
 			vector.setY((-2 * x * y * z + 2 * y * z) / (2 * (z * z + y * y)));
-			vector.setZ((-x * z - y * vector.y) / z);
+			vector.setZ((-x * z - y * vector.getY()) / z);
 			return vector;
 		}
 		return vector;
 	}
 
 	/**
-	 * Adds a vector3 to this vector, then returns the result.
-	 * Does not create a new vector.
-	 * 
+	 * Normalizes the vector. Doesn't create a new vector.
+	 *
+	 * @return This vector after normalization
+	 */
+	public Vector3 normalize() {
+		if (isZero()) {
+			throw new UnsupportedOperationException("Unable to normalize the zero vector");
+		}
+		final float length = length();
+		x /= length;
+		y /= length;
+		z /= length;
+		return this;
+	}
+
+	/**
+	 * Return the corresponding absolute value vector. Creates a new vector.
+	 *
+	 * @return new {@link Vector3} absolute value vector
+	 */
+	public Vector3 getAbsoluteVector() {
+		return new Vector3(
+				Math.abs(x),
+				Math.abs(y),
+				Math.abs(z));
+	}
+
+	/**
+	 * Scalar product of two vectors
+	 *
+	 * @param vector to compute scalar product with
+	 * @return {@link float} scalar product
+	 */
+	public float dot(Vector3 vector) {
+		return x * vector.getX() + y * vector.getY() + z * vector.getZ();
+	}
+
+	/**
+	 * Crosses a vector3 with this vector. Creates a new vector.
+	 *
+	 * @param vector to compute the cross product with
+	 * @return a new vector, result of the cross product
+	 */
+	public Vector3 cross(Vector3 vector) {
+		return new Vector3(
+				y * vector.getZ() - z * vector.getY(),
+				z * vector.getX() - x * vector.getZ(),
+				x * vector.getY() - y * vector.getX());
+	}
+
+	/**
+	 * Adds a vector3 to this vector, then returns the result. Does not create a new vector.
+	 *
 	 * @param vector to add to this one
-	 * @return this vector, after adding is finished
+	 * @return this vector, after addition is finished
 	 */
 	public Vector3 add(Vector3 vector) {
-		x += vector.x;
-		y += vector.y;
-		z += vector.z;
+		x += vector.getX();
+		y += vector.getY();
+		z += vector.getZ();
 		return this;
 	}
 
 	/**
-	 * Subtracts a vector3 from this vector, then returns the result.
-	 * Does not create a new vector.
-	 * 
+	 * Negates the components of this vector, then returns the result. Does not create a new vector.
+	 *
+	 * @return this vector, after negation is finished
+	 */
+	public Vector3 negate() {
+		x = -x;
+		y = -y;
+		z = -z;
+		return this;
+	}
+
+	/**
+	 * Subtracts a vector3 from this vector, then returns the result. Does not create a new vector.
+	 *
 	 * @param vector to subtract from this one
-	 * @return this vector, after subtraction is finished
+	 * @return the difference of this vector and the other vector
 	 */
 	public Vector3 subtract(Vector3 vector) {
-		x -= vector.x;
-		y -= vector.y;
-		z -= vector.z;
+		x -= vector.getX();
+		y -= vector.getY();
+		z -= vector.getZ();
 		return this;
 	}
 
 	/**
-	 * Multiplies this vector by a specified amount.
-	 * 
-	 * @param amount to multiply by
+	 * Multiplies this vector by a specified value. Does not create a new vector.
+	 *
+	 * @param value to multiply by
 	 * @return this vector, after multiplication is finished
 	 */
-	public Vector3 multiply(float amount) {
-		x *= amount;
-		y *= amount;
-		z *= amount;
+	public Vector3 multiply(float value) {
+		x *= value;
+		y *= value;
+		z *= value;
+		return this;
+	}
+
+	/**
+	 * Divides this vector by a specified value. Does not create a new vector.
+	 *
+	 * @param value to multiply by
+	 * @return this vector, after division is finished
+	 */
+	public Vector3 divide(float value) {
+		x /= value;
+		y /= value;
+		z /= value;
 		return this;
 	}
 
 	/**
 	 * Gets the corresponding float value from this vector based on the requested axis.<br><br>
-	 * 
-	 * Valid axis are:<br>
-	 * {@link Vector3#X_AXIS}<br>
-	 * {@link Vector3#Y_AXIS}<br>
+	 *
+	 * Valid axis are:<br> {@link Vector3#X_AXIS}<br> {@link Vector3#Y_AXIS}<br>
 	 * {@link Vector3#Z_AXIS}<br>
-	 * 
-	 * @param axis to get; {@link Vector3#X_AXIS} OR {@link Vector3#Y_AXIS} OR {@link Vector3#Z_AXIS}
+	 *
+	 * @param axis to get; {@link Vector3#X_AXIS} OR {@link Vector3#Y_AXIS} OR
+	 * {@link Vector3#Z_AXIS}
 	 * @return {@link float} value of the axis
 	 */
-	public float getAxis(int axis) {
-		if (axis == X_AXIS) {
-			return x;
+	public float get(int axis) {
+		switch (axis) {
+			case X_AXIS:
+				return x;
+			case Y_AXIS:
+				return y;
+			case Z_AXIS:
+				return z;
 		}
-
-		if (axis == Y_AXIS) {
-			return y;
-		}
-
-		if (axis == Z_AXIS) {
-			return z;
-		}
-
 		throw new UnsupportedOperationException("Must specify 0, 1, or 2 as an axis. (Vector3.X_AXIS, Vector3.Y_AXIS, Vector3.Z_AXIS");
 	}
+
+	/**
+	 * Sets the corresponding float value from this vector based on the requested axis.<br><br>
+	 *
+	 * Valid axis are:<br> {@link Vector3#X_AXIS}<br> {@link Vector3#Y_AXIS}<br>
+	 * {@link Vector3#Z_AXIS}<br>
+	 *
+	 * @param axis to set; {@link Vector3#X_AXIS} OR {@link Vector3#Y_AXIS} OR
+	 * {@link Vector3#Z_AXIS}
+	 * @param value {@link float} value for the axis
+	 */
+	public float set(int axis, float value) {
+		switch (axis) {
+			case X_AXIS:
+				x = value;
+				break;
+			case Y_AXIS:
+				y = value;
+				break;
+			case Z_AXIS:
+				z = value;
+		}
+		throw new UnsupportedOperationException("Must specify 0, 1, or 2 as an axis. (Vector3.X_AXIS, Vector3.Y_AXIS, Vector3.Z_AXIS");
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -370,19 +439,91 @@ public class Vector3 {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (!(obj instanceof Vector3)) {
 			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		}
 		Vector3 other = (Vector3) obj;
-		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x)) {
 			return false;
-		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+		}
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y)) {
 			return false;
-		if (Float.floatToIntBits(z) != Float.floatToIntBits(other.z))
+		}
+		if (Float.floatToIntBits(z) != Float.floatToIntBits(other.z)) {
 			return false;
+		}
 		return true;
+	}
+
+	/**
+	 * Adds a vector3 to another vector3. Creates a new vector.
+	 *
+	 * @param vector1 the first vector
+	 * @param vector2 the second vector
+	 * @return the sum of the two vectors
+	 */
+	public static Vector3 add(Vector3 vector1, Vector3 vector2) {
+		return new Vector3(
+				vector1.getX() + vector2.getX(),
+				vector1.getY() + vector2.getY(),
+				vector1.getZ() + vector2.getZ());
+	}
+
+	/**
+	 * Negates the components of this vector. Creates a new vector.
+	 *
+	 * @param vector the vector to negate
+	 * @return the negative vector for this vector
+	 */
+	public static Vector3 negate(Vector3 vector) {
+		return new Vector3(
+				-vector.getX(),
+				-vector.getY(),
+				-vector.getZ());
+	}
+
+	/**
+	 * Subtracts a vector3 from another vector3. Creates a new vector.
+	 *
+	 * @param vector1 the first vector
+	 * @param vector2 the second vector
+	 * @return the difference of the two vectors
+	 */
+	public static Vector3 subtract(Vector3 vector1, Vector3 vector2) {
+		return new Vector3(
+				vector1.getX() - vector2.getX(),
+				vector1.getY() - vector2.getY(),
+				vector1.getZ() - vector2.getZ());
+	}
+
+	/**
+	 * Multiplies the vector by a specified value. Creates a new vector.
+	 *
+	 * @param vector the vector
+	 * @param value the value
+	 * @return the product of the vector and the value
+	 */
+	public static Vector3 multiply(Vector3 vector, float value) {
+		return new Vector3(
+				vector.getX() * value,
+				vector.getY() * value,
+				vector.getZ() * value);
+	}
+
+	/**
+	 * Divides this vector by a specified value. Creates a new vector.
+	 *
+	 * @param vector the vector
+	 * @param value the value
+	 * @return the quotient (vector3) of the vector and the value
+	 */
+	public static Vector3 divide(Vector3 vector, float value) {
+		return new Vector3(
+				vector.getX() / value,
+				vector.getY() / value,
+				vector.getZ() / value);
 	}
 }
