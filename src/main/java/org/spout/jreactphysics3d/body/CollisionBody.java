3f97975952be57790e784b881194f137552c2412
Aleksi Sapon <alek42008@yahoo.com>
2013-05-25 16:38:01 -0400

    Port CollisionBody
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/jreactphysics3d/body/CollisionBody.java
+++ src/main/java/org/spout/jreactphysics3d/body/CollisionBody.java
@@ -26,5 +26,201 @@
  */
 package org.spout.jreactphysics3d.body;
 
-public class CollisionBody {
+import org.spout.jreactphysics3d.collision.shape.AABB;
+import org.spout.jreactphysics3d.collision.shape.CollisionShape;
+import org.spout.jreactphysics3d.mathematics.Transform;
+
+/**
+ * Represents a body that is able to collide with others bodies. This class inherits from the Body
+ * class.
+ */
+public class CollisionBody extends Body {
+	protected CollisionShape mCollisionShape;
+	protected final Transform mTransform = new Transform();
+	protected final Transform mOldTransform = new Transform();
+	protected float mInterpolationFactor;
+	protected boolean mIsActive;
+	protected boolean mIsMotionEnabled;
+	protected boolean mIsCollisionEnabled;
+	protected AABB mAabb;
+	protected boolean mHasMoved;
+
+	/**
+	 * Constructs a new collision body from it's transform, collision shape, and ID.
+	 *
+	 * @param transform The transform
+	 * @param collisionShape The collision shape
+	 * @param id The ID
+	 */
+	public CollisionBody(Transform transform, CollisionShape collisionShape, long id) {
+		super(id);
+		if (collisionShape == null) {
+			throw new IllegalArgumentException("collisionShape cannot be null");
+		}
+		mTransform.set(transform);
+		mCollisionShape = collisionShape;
+		mIsActive = true;
+		mHasMoved = false;
+		mIsMotionEnabled = true;
+		mIsCollisionEnabled = true;
+		mInterpolationFactor = 0;
+		mOldTransform.set(transform);
+		mCollisionShape.updateAABB(mAabb, transform);
+	}
+
+	/**
+	 * Returns true if the body has moved during the last frame, false if not.
+	 *
+	 * @return Whether or not the body has moved in the last frame
+	 */
+	public boolean getHasMoved() {
+		return mHasMoved;
+	}
+
+	/**
+	 * Set the hasMoved variable (true if the body has moved during the last frame).
+	 *
+	 * @param mHasMoved Whether or not the body has moved in the last frame
+	 */
+	public void setHasMoved(boolean mHasMoved) {
+		this.mHasMoved = mHasMoved;
+	}
+
+	/**
+	 * Gets the collision shape.
+	 *
+	 * @return The collision shape
+	 */
+	public CollisionShape getCollisionShape() {
+		return mCollisionShape;
+	}
+
+	/**
+	 * Sets the collision shape
+	 *
+	 * @param mCollisionShape The collision shape to set
+	 */
+	public void setCollisionShape(CollisionShape mCollisionShape) {
+		this.mCollisionShape = mCollisionShape;
+	}
+
+	/**
+	 * Returns true if the body is active, false if not.
+	 *
+	 * @return Whether or not the body is active
+	 */
+	public boolean getIsActive() {
+		return mIsActive;
+	}
+
+	/**
+	 * Sets the activity for this body. True for active, false for inactive.
+	 *
+	 * @param mIsActive True if this body is active, false if not
+	 */
+	public void setIsActive(boolean mIsActive) {
+		this.mIsActive = mIsActive;
+	}
+
+	/**
+	 * Returns an interpolated body from the old to the current transform, based on this body's
+	 * interpolation factor.
+	 *
+	 * @return A transform interpolated from the old to the current transform based on the
+	 *         interpolation factor
+	 */
+	public Transform getInterpolatedTransform() {
+		return Transform.interpolateTransforms(mOldTransform, mTransform, mInterpolationFactor);
+	}
+
+	/**
+	 * Sets the interpolation factor for the transforms of this body.
+	 *
+	 * @param factor The interpolation factor
+	 */
+	public void setInterpolationFactor(float factor) {
+		mInterpolationFactor = factor;
+	}
+
+	/**
+	 * Returns true if the body can move, false if not.
+	 *
+	 * @return Whether or not the body can move
+	 */
+	public boolean getIsMotionEnabled() {
+		return mIsMotionEnabled;
+	}
+
+	/**
+	 * Sets whether or not this body can move. True to allow movement, false to disallow.
+	 *
+	 * @param isMotionEnabled True if the body should move, false if not
+	 */
+	public void setIsMotionEnabled(boolean isMotionEnabled) {
+		mIsMotionEnabled = isMotionEnabled;
+	}
+
+	/**
+	 * Gets this body's current position and orientation as a transform.
+	 *
+	 * @return The body's transform
+	 */
+	public Transform getTransform() {
+		return mTransform;
+	}
+
+	/**
+	 * Sets this body's current position and orientation as a transform.
+	 *
+	 * @param transform The transform to set for this body
+	 */
+	public void setTransform(Transform transform) {
+		if (!mTransform.equals(transform)) {
+			mHasMoved = true;
+		}
+		mTransform.set(transform);
+	}
+
+	/**
+	 * Gets this body's axis-aligned bounding box (AABB).
+	 *
+	 * @return The body's AABB
+	 */
+	public AABB getAABB() {
+		return mAabb;
+	}
+
+	/**
+	 * Return true if the body can collide with others bodies.
+	 *
+	 * @return Whether or not this body can collide with others
+	 */
+	public boolean getIsCollisionEnabled() {
+		return mIsCollisionEnabled;
+	}
+
+	/**
+	 * Sets whether or not this body can collide. True to allow collisions, false to disallow.
+	 *
+	 * @param isCollisionEnabled True if the body should collide, false if not
+	 */
+	public void setIsCollisionEnabled(boolean isCollisionEnabled) {
+		mIsCollisionEnabled = isCollisionEnabled;
+	}
+
+	/**
+	 * Update the old transform with the current one. This is used to compute the interpolated position
+	 * and orientation of the body.
+	 */
+	public void updateOldTransform() {
+		mOldTransform.set(mTransform);
+	}
+
+	/**
+	 * Update the rigid body in order to reflect a change in the body state.
+	 */
+	public void updateAABB() {
+		// TODO : An AABB should not be updated every frame but only if the body has moved
+		mCollisionShape.updateAABB(mAabb, mTransform);
+	}
 }
