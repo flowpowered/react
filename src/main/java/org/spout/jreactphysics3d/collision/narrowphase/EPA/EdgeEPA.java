cb7e747ccd37db18433c303fde09c8dba629289b
Aleksi Sapon <alek42008@yahoo.com>
2013-05-31 17:25:22 -0400

    Port EdgeEPA
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/jreactphysics3d/collision/narrowphase/EPA/EdgeEPA.java
+++ src/main/java/org/spout/jreactphysics3d/collision/narrowphase/EPA/EdgeEPA.java
@@ -26,6 +26,156 @@
  */
 package org.spout.jreactphysics3d.collision.narrowphase.EPA;
 
-public class EdgeEPA {
+import org.spout.jreactphysics3d.mathematics.Vector3;
 
+/**
+ * Represents an edge for the current polytope in the EPA algorithm.
+ */
+public class EdgeEPA {
+	private TriangleEPA mOwnerTriangle;
+	private int mIndex;
+
+	/**
+	 * Default constructor. The owner triangle and index are null.
+	 */
+	public EdgeEPA() {
+	}
+
+	/**
+	 * Construct a new edge for the EPA from its owner triangle and index.
+	 *
+	 * @param ownerTriangle The owner triangle
+	 * @param index The index
+	 */
+	public EdgeEPA(TriangleEPA ownerTriangle, int index) {
+		if (index < 0 || index >= 3) {
+			throw new IllegalArgumentException("index must be greater or equal to zero and smaller than three");
+		}
+		mOwnerTriangle = ownerTriangle;
+		mIndex = index;
+	}
+
+	/**
+	 * Copy constructor.
+	 *
+	 * @param edge The edge to copy
+	 */
+	public EdgeEPA(EdgeEPA edge) {
+		mOwnerTriangle = edge.mOwnerTriangle;
+		mIndex = edge.mIndex;
+	}
+
+	/**
+	 * Gets the owner triangle.
+	 *
+	 * @return The owner triangle
+	 */
+	public TriangleEPA getOwnerTriangle() {
+		return mOwnerTriangle;
+	}
+
+	/**
+	 * Gets the edge index.
+	 *
+	 * @return The edge index
+	 */
+	public int getIndex() {
+		return mIndex;
+	}
+
+	/**
+	 * Sets the values of this edge to the ones of the provided edge.
+	 *
+	 * @param edge The edge to copy the values from
+	 */
+	public void set(EdgeEPA edge) {
+		mOwnerTriangle = edge.mOwnerTriangle;
+		mIndex = edge.mIndex;
+	}
+
+	/**
+	 * Gets the index of the source vertex for the edge (vertex starting the edge).
+	 *
+	 * @return The index of the source vertex
+	 */
+	public int getSourceVertexIndex() {
+		return mOwnerTriangle.get(mIndex);
+	}
+
+	/**
+	 * Gets the index of the target vertex for the edge (vertex ending the edge).
+	 *
+	 * @return The index of the target vertex
+	 */
+	public int getTargetVertexIndex() {
+		return mOwnerTriangle.get(indexOfNextCounterClockwiseEdge(mIndex));
+	}
+
+	/**
+	 * Executes the recursive silhouette algorithm from this edge.
+	 *
+	 * @param vertices The vertices
+	 * @param indexNewVertex The index of the new vertex
+	 * @param triangleStore The triangle store
+	 * @return True if the owner triangle was obsolete or if a new triangle edge was half liked with
+	 *         this one
+	 */
+	public boolean computeSilhouette(Vector3[] vertices, int indexNewVertex, TrianglesStore triangleStore) {
+		if (!mOwnerTriangle.getIsObsolete()) {
+			if (!mOwnerTriangle.isVisibleFromVertex(vertices, indexNewVertex)) {
+				TriangleEPA triangle = triangleStore.newTriangle(
+						vertices,
+						indexNewVertex,
+						getTargetVertexIndex(),
+						getSourceVertexIndex());
+				if (triangle != null) {
+					TriangleEPA.halfLink(new EdgeEPA(triangle, 1), this);
+					return true;
+				}
+				return false;
+			} else {
+				mOwnerTriangle.setIsObsolete(true);
+				int backup = triangleStore.getNbTriangles();
+				if (!mOwnerTriangle.getAdjacentEdge(indexOfNextCounterClockwiseEdge(this.mIndex))
+						.computeSilhouette(vertices, indexNewVertex, triangleStore)) {
+					mOwnerTriangle.setIsObsolete(false);
+					TriangleEPA triangle = triangleStore.newTriangle(
+							vertices,
+							indexNewVertex,
+							getTargetVertexIndex(),
+							getSourceVertexIndex());
+					if (triangle != null) {
+						TriangleEPA.halfLink(new EdgeEPA(triangle, 1), this);
+						return true;
+					}
+					return false;
+				} else if (!mOwnerTriangle.getAdjacentEdge(indexOfPreviousCounterClockwiseEdge(this.mIndex))
+						.computeSilhouette(vertices, indexNewVertex, triangleStore)) {
+					mOwnerTriangle.setIsObsolete(false);
+					triangleStore.setNbTriangles(backup);
+					TriangleEPA triangle = triangleStore.newTriangle(
+							vertices,
+							indexNewVertex,
+							getTargetVertexIndex(),
+							getSourceVertexIndex());
+					if (triangle != null) {
+						TriangleEPA.halfLink(new EdgeEPA(triangle, 1), this);
+						return true;
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	// Returns the index of the next counter-clockwise edge of the owner triangle.
+	private static int indexOfNextCounterClockwiseEdge(int i) {
+		return (i + 1) % 3;
+	}
+
+	// Returns the index of the previous counter-clockwise edge of the owner triangle.
+	private static int indexOfPreviousCounterClockwiseEdge(int i) {
+		return (i + 2) % 3;
+	}
 }
